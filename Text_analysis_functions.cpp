3c2575df4d281bcccf18bd9f9c756dd844aed0db
DoomLordn <DoomLordn@hotmail.com>
2019-04-14 17:54:26 +0300

    Замена имён переменных

--- Text_analysis_functions.cpp
+++ Text_analysis_functions.cpp
@@ -115,18 +115,18 @@ void delete_repetitions(vector <string>& array_to_sort, vector <int>& frequency)
 
 void sort_array(vector <string>& array_to_sort, vector <int>& frequency) {
 	bool flag = true;
-	string y;
-	int x;
+	string buf_str;
+	int buf_int;
 	while (flag) {
 		flag = false;
 		for (size_t i = 0; i < frequency.size() - 1; i++) {
 			if (frequency[i] < frequency[i + 1]) {
-				x = frequency[i];
+				buf_int = frequency[i];
 				frequency[i] = frequency[i + 1];
-				frequency[i + 1] = x;
-				y = array_to_sort[i];
+				frequency[i + 1] = buf_int;
+				buf_str= array_to_sort[i];
 				array_to_sort[i] = array_to_sort[i + 1];
-				array_to_sort[i + 1] = y;
+				array_to_sort[i + 1] = buf_str;
 				flag = true;
 			}
 		}
